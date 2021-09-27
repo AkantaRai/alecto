d94a703b9dd569ac65917ac2be64cb435cd5b541
AkantaRai <ekantarai10@gmail.com>
2021-09-27 14:45:11 +0545

    done

---
+++ lab 3/q15.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    int num,i,count=0;
+    printf("enter a number:");
+    scanf("%d",&num);
+    for(i=1;i<100;i++){
+        for(j=1;j<i;j++){
+            if(i)
+        }
+    }
+    return 0;
+}
